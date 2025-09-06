914bf3c50dfbb462aec8bba018f643c5ca9d1cc1
PriyanshuShekhar10 <62722200+PriyanshuShekhar10@users.noreply.github.com>
2025-09-06 15:26:38 +0530

    typo

--- components/hero.tsx
+++ components/hero.tsx
@@ -21,17 +21,12 @@ export function Hero() {
             </span>{" "}
             a little more productive
           </h1>
-
-          {/* <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-            Get early access
-          </button> */}
           <div className="flex flex-col items-center mt-6 space-y-3">
             {/* Button */}
             <button className="mb-64 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
               Get early access
             </button>
 
-            {/* Waitlist section */}
             <div className="flex items-center space-x-3">
               <div className="flex -space-x-2">
                 <img
